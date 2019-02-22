002e00392209e7e79bd299bc8fb5ccf1542a3c35
Administrator <Administrator@SD-20181229CKLG>
2019-02-22 15:32:33 +0800

    优化代码

--- src/main/java/com/example/controller/HelloWorldController.java
+++ src/main/java/com/example/controller/HelloWorldController.java
@@ -1,26 +1,16 @@
 package com.example.controller;
 
-import org.jboss.logging.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.netflix.discovery.DiscoveryClient;
 
 @EnableAutoConfiguration
 @RestController
 @Component
 public class HelloWorldController {
-	
-//	private final Logger logger = Logger.getLogger(getClass());
-//	
-//	@Autowired
-//	private DiscoveryClient client;
-	
 
-	@RequestMapping(value = "/hello",method = RequestMethod.GET)
+	@GetMapping(value = "/hello")
 	public String index() {
 		return "Hello World";
 	}
